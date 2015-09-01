2573aa6efdcee8d90342fd26e2c68e73f21c0301
Shlomi Assaf <shlomiasaf@gmail.com>
2015-09-02 00:45:12 +0300

    readme.md mostly

--- src/abc.ts
+++
@@ -1,4 +0,0 @@
-import * as t from 'typescript';
-import * as r from 'reflect-metadata';
-export var x = r;
-export var y = t;
--- src/core/annotations/view.ts
+++
@@ -1,3 +0,0 @@
-/* SEE: https://github.com/angular/angular/blob/master/modules/angular2/src/core/annotations/view.ts */
-
-export {View as ViewAnnotation} from '../annotations_impl/view';
